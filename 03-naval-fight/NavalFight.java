8626de8adf3fce9ac5eedbd73929bd65a478e54a
Vasco Santos <jvosantos@gmail.com.com>
2018-10-03 13:05:19 +0100

    adding final naval fight exercise

--- 03-naval-fight/NavalFight.java
+++ 03-naval-fight/NavalFight.java
@@ -43,7 +43,44 @@ public class NavalFight {
         askShip(board, 1);
         printBoard(board);
 
-        printBoard(board);
+        while(shipSquaresLeft(board) > 0) {
+            askBomb(board);
+            printBoard(board);
+        }
+    }
+
+    public static int shipSquaresLeft(char[][] board) {
+        int countSquareShips = 0;
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                if(board[i][j] == 'V') {
+                    countSquareShips++;
+                }
+            }
+        }
+
+        if(debug) {
+            System.out.printf("DEBUG: %d square ships left\n", countSquareShips);
+        }
+
+        return countSquareShips;
+    }
+
+    public static void askBomb(char[][] board) {
+        System.out.print("Line: ");
+        int line = sc.nextInt();
+
+        System.out.print("Column: ");
+        int column = sc.nextInt();
+
+        if(board[line][column] == '_') {
+            System.out.println("WATER!!!!!");
+            board[line][column] = 'O';
+        } else {
+            System.out.println("HIT!!!!!");
+            board[line][column] = '\u2718';
+        }
+
     }
 
     public static void initBoard(char[][] board) {
@@ -54,19 +91,74 @@ public class NavalFight {
         }
     }
 
-    public static void askShip(char[][] board, int numberOfSquares) {
+    public static void askShipV2(char[][] board, int numberOfSquares) {
         System.out.printf("Start of ship with %d squares\n", numberOfSquares);
 
-        System.out.print("line: ");
-        int startLine = sc.nextInt();
+        System.out.print("coordinate: ");
+        String coordinate = sc.nextLine(); // XN
 
-        System.out.print("column: ");
-        int startColumn = sc.nextInt();
+        if(debug) {
+            System.out.println("DEBUG: coordinate: " + coordinate);
+            System.out.println("line: " + coordinate.charAt(1));
+            System.out.println("column: " + coordinate.charAt(0));
+        }
 
-        System.out.println("Orientation of ship");
-        System.out.println("1 - vertical");
-        System.out.println("2 - horizontal");
-        int option = sc.nextInt();
+        int startLine = Integer.valueOf(coordinate.charAt(1) + ""); // N
+        char column = coordinate.charAt(0); // X
+
+        int startColumn = 0;
+        switch(column) {
+            case 'A':
+            case 'a':
+                startColumn = 0;
+                break;
+            case 'B':
+            case 'b':
+                startColumn = 1;
+                break;
+            case 'C':
+            case 'c':
+                startColumn = 2;
+                break;
+            case 'D':
+            case 'd':
+                startColumn = 3;
+                break;
+            case 'E':
+            case 'e':
+                startColumn = 4;
+                break;
+            case 'F':
+            case 'f':
+                startColumn = 5;
+                break;
+            case 'G':
+            case 'g':
+                startColumn = 6;
+                break;
+            case 'H':
+            case 'h':
+                startColumn = 7;
+                break;
+            case 'I':
+            case 'i':
+                startColumn = 8;
+                break;
+            case 'J':
+            case 'j':
+                startColumn = 9;
+                break;
+        }
+
+        int option = 2;
+        System.out.println("Hello");
+        if(numberOfSquares != 1) {
+            System.out.println("Orientation of ship");
+            System.out.println("1 - vertical");
+            System.out.println("2 - horizontal");
+            sc.nextInt();
+        }
+        System.out.println("goodbye");
 
         int endLine;
         int endColumn;
@@ -84,10 +176,10 @@ public class NavalFight {
         }
 
         if(debug) {
-            System.out.printf("startLine: %d\n", startLine);
-            System.out.printf("startColumn: %d\n", startColumn);
-            System.out.printf("endLine: %d\n", endLine);
-            System.out.printf("endColumn: %d\n", endColumn);
+            System.out.printf("DEBUG: startLine: %d\n", startLine);
+            System.out.printf("DEBUG: startColumn: %d\n", startColumn);
+            System.out.printf("DEBUG: endLine: %d\n", endLine);
+            System.out.printf("DEBUG: endColumn: %d\n", endColumn);
         }
 
         // board[startLine][startColumn] = '\u2718';
@@ -96,9 +188,63 @@ public class NavalFight {
         for(int i = startLine; i <= endLine; i++) {
             for(int j = startColumn; j <= endColumn; j++) {
                 if(debug) {
-                    System.out.printf("board[%d][%d] = \u2718\n", i, j);
+                    System.out.printf("DEBUG: board[%d][%d] = V\n", i, j);
                 }
-                board[i][j] = '\u2718';
+                board[i][j] = 'V';
+            }
+        }
+
+    }
+
+
+    public static void askShip(char[][] board, int numberOfSquares) {
+        System.out.printf("Start of ship with %d squares\n", numberOfSquares);
+
+        System.out.print("line: ");
+        int startLine = sc.nextInt();
+
+        System.out.print("column: ");
+        int startColumn = sc.nextInt();
+
+        int option = 2;
+        if(numberOfSquares != 1) {
+            System.out.println("Orientation of ship");
+            System.out.println("1 - vertical");
+            System.out.println("2 - horizontal");
+            sc.nextInt();
+        }
+
+        int endLine;
+        int endColumn;
+
+        switch(option) {
+            case 1:
+                endLine = startLine + numberOfSquares - 1;
+                endColumn = startColumn;
+                break;
+            case 2:
+            default:
+                endLine = startLine;
+                endColumn = startColumn + numberOfSquares - 1;
+                break;
+        }
+
+        if(debug) {
+            System.out.printf("DEBUG: startLine: %d\n", startLine);
+            System.out.printf("DEBUG: startColumn: %d\n", startColumn);
+            System.out.printf("DEBUG: endLine: %d\n", endLine);
+            System.out.printf("DEBUG: endColumn: %d\n", endColumn);
+        }
+
+        // board[startLine][startColumn] = '\u2718';
+        // fill middle
+        // board[endLine][endColumn] = '\u2718';
+        for(int i = startLine; i <= endLine; i++) {
+            for(int j = startColumn; j <= endColumn; j++) {
+                if(debug) {
+                    System.out.printf("DEBUG: board[%d][%d] = V\n", i, j);
+                }
+                board[i][j] = 'V';
             }
         }
 
